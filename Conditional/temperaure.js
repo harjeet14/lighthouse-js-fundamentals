61997ba9b807e62f966da86380c08e976ae46ca2
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-05-23 16:42:07 -0400

    Temperature condition

---
+++ Conditional/temperaure.js
@@ -0,0 +1,9 @@
+const temp = 33;
+if (temp < 0) {
+    console.log("Make sure you pick out a shawl.")
+} else if (temp < 12) {
+    console.log("Half sleevs are not enough.")
+} else {
+    console.log("Short sleevs are fine.")
+}
+console.log("Now you are ready to go outside")
