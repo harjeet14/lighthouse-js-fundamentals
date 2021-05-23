34b77c3524f771a081db2b75eba4e23eb9311cf5
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-05-23 14:42:15 -0400

    conditions checking program

---
+++ conditionals.js
@@ -0,0 +1,9 @@
+const raining = true;
+const cold = true;
+if (raining) {
+    console.log("Don't forget your umbrella!");
+}
+if (cold) {
+    console.log("Make sure you picked out a scarf.");
+}
+console.log("Now you are ready to go outside!")
